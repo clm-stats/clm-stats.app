b2767c608fff0494ffaa77c6c6e2eaf845cedabd
Mitch Dzugan <mitchdzugan@gmail.com>
2026-03-10 10:08:09 +0000

    explicitly empty out dirs

--- vite.client.mjs
+++ vite.client.mjs
@@ -10,6 +10,7 @@ export default defineConfig({
   plugins: [preact()],
   build: {
     minify: "terser",
+    emptyOutDir: true,
     outDir: `${__dirname}/dist`,
     lib: {
       entry: `${__dirname}/js/mountClient.js`,
